9de33dc8f4e649931079b36847b44cd0195105c3
kyracode <95079037+kyracode@users.noreply.github.com>
2022-01-29 17:44:47 +0800

    Update BouncyBall.js

--- BouncyBall.js
+++ BouncyBall.js
@@ -13,11 +13,11 @@ class BouncyBall{
 		// 	density:20
 		// }
 
-		// var options={
-		// 	restitution:0.3,
-		// 	friction:5,
-		// 	density:1
-		// }
+		 var options={
+		 	restitution:0.3,
+		 	friction:5,
+		 	density:1
+		 }
 
 		// var options{
 		// 	restitution:5,
@@ -29,7 +29,7 @@ class BouncyBall{
 		this.x=x;
 		this.y=y;
 		this.r=r
-		// this.body=Bodies.ellipse(this.x, this.y, (this.r-20)/2, options)
+		 this.body=Bodies.ellipse(this.x, this.y, (this.r-20)/2, options)
 		// World.add(world, this.body);
 
 		// this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
@@ -52,7 +52,7 @@ class BouncyBall{
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
-			// ellipse(0,0,this.r, this.r);
+			 ellipse(0,0,this.r, this.r);
 			// circle(0,0,this.r, this.r);
 			// rect(0,0,this.r, this.r);
 			// body(0,0,this.r, this.r);
