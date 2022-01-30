66c4800e3c07f1ee7e68a05448e1d726f1b6fdd4
kyracode <95079037+kyracode@users.noreply.github.com>
2022-01-30 14:14:23 +0800

    Update BouncyBall.js

--- BouncyBall.js
+++ BouncyBall.js
@@ -30,7 +30,7 @@ class BouncyBall{
 		this.y=y;
 		this.r=r
 		 this.body=Bodies.ellipse(this.x, this.y, (this.r-20)/2, options)
-		// World.add(world, this.body);
+		World.add(world, this.body);
 
 		// this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
 		// World.add(world, this.body);
